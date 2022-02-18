1592e9e29c5733e220dfab98a94e8dd33b7d1cdd
Ruwan <ruwan@solana.fm>
2022-02-18 12:05:35 +0800

    Updated stylings and redirecting on user settings page
    Signed-off-by: Ruwan <ruwan@solana.fm>

--- pages/userSettings.tsx
+++ pages/userSettings.tsx
@@ -1,17 +1,22 @@
+import NavBar from "../components/NavBar"
+
 const UserSettings = () => {
     return (
-        <div className="flex justify-center w-full h-full pt-40">
-            <div className="flex flex-col">
-                <div className="text-md font-normal border-b-2 border-purple-300 px-8">
-                    There is nothing much to see here so i suggest you go back to swiping
+        <>
+            <NavBar/>
+            <div className="flex justify-center w-full h-full pt-40">
+                <div className="flex flex-col">
+                    <div className="text-md font-normal border-b-2 border-purple-300 px-8">
+                        There is nothing much to see here so i suggest you go back to swiping
+                    </div>
+                    <div className="py-1"></div>
+                    <a className="rounded-lg flex justify-center pt-4 pb-2 text-lg font-semibold bg-gradient-to-r from-indigo-400 hover:text-white" href="/swipe">
+                        Take me back. Please...
+                    </a>
                 </div>
-                <div className="py-1"></div>
-                <a className="rounded-lg flex justify-center pt-4 pb-2 text-lg font-semibold bg-gradient-to-r from-indigo-400 hover:text-white" href="/swipe">
-                    Take me back. Please...
-                </a>
             </div>
+        </>
 
-        </div>
     )
 }
 
